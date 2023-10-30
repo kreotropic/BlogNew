b34a76774ec8e170dff9d99b15aa88df1e449883
Vasco Silva <vasco.silva.t0123187@edu.atec.pt>
2023-10-30 12:40:31 +0000

    Added username to register and login - New users automatically assigned User role

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -75,7 +75,7 @@ namespace BlogNew.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -151,10 +151,13 @@ namespace BlogNew.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //Add role "User" to user
+                    UserManager.AddToRole(user.Id, "User");
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -48,12 +48,18 @@ namespace BlogNew.Models
 
     public class LoginViewModel
     {
-        [Required]
+        /*[Required]
         [Display(Name = "Email")]
         [EmailAddress]
-        public string Email { get; set; }
+        public string Email { get; set; }*/
 
-        [Required]
+        [Required(ErrorMessage = "Username is required.")]
+        [Display(Name = "Username")]
+        [RegularExpression(@"^[a-zA-Z0-9]{1,10}$",
+         ErrorMessage = "Username must be up to 10 uppercase or lowercase letters or digits.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
@@ -64,12 +70,18 @@ namespace BlogNew.Models
 
     public class RegisterViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Username is required.")]
+        [Display(Name = "Username")]
+        [RegularExpression(@"^[a-zA-Z0-9]{1,10}$",
+         ErrorMessage = "Username must be up to 10 uppercase or lowercase letters or digits.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required.")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
