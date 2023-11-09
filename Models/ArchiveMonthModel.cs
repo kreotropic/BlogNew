e0d8b4ef33aaa44d487a81291606bfeb2a7ea6c1
Vasco Silva <vasco.silva.t0123187@edu.atec.pt>
2023-11-09 00:52:05 +0000

    Archive View Completed

--- Models/ArchiveMonthModel.cs
+++ Models/ArchiveMonthModel.cs
@@ -8,6 +8,7 @@ namespace BlogNew.Models
     public class ArchiveMonthModel
     {
         public string Month { get; set; }
+        public int TotalPosts { get { return Posts.Count; } }
         public List<ArchivePostModel> Posts { get; set; } = new List<ArchivePostModel>();
     }
 }
